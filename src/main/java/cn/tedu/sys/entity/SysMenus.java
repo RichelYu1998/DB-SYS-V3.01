87955182bc90871427feb5d4fdbdcf6a9296644d
余浩 <980187223@qq.com>
2020-07-20 10:07:50 +0800

    entity细节修改

--- src/main/java/cn/tedu/sys/entity/SysMenus.java
+++ src/main/java/cn/tedu/sys/entity/SysMenus.java
@@ -39,7 +39,7 @@ public class SysMenus implements Serializable {
     /**
      * 父菜单ID，一级菜单为0
      */
-    private Integer parentid;
+    private Integer parentId;
     /**
      * 授权(如：user:create)
      */
@@ -59,103 +59,8 @@ public class SysMenus implements Serializable {
     /**
      * 修改用户
      */
-    private String modifieduser;
+    private String modifiedUser;
 
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public Integer getSort() {
-        return sort;
-    }
-
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public Integer getParentid() {
-        return parentid;
-    }
-
-    public void setParentid(Integer parentid) {
-        this.parentid = parentid;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
-
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-
-    public Date getCreatedtime() {
-        return createdtime;
-    }
-
-    public void setCreatedtime(Date createdtime) {
-        this.createdtime = createdtime;
-    }
-
-    public Date getModifiedtime() {
-        return modifiedtime;
-    }
-
-    public void setModifiedtime(Date modifiedtime) {
-        this.modifiedtime = modifiedtime;
-    }
-
-    public String getCreateduser() {
-        return createduser;
-    }
-
-    public void setCreateduser(String createduser) {
-        this.createduser = createduser;
-    }
-
-    public String getModifieduser() {
-        return modifieduser;
-    }
-
-    public void setModifieduser(String modifieduser) {
-        this.modifieduser = modifieduser;
-    }
 
 }
--- src/main/java/cn/tedu/sys/entity/SysUsers.java
+++ src/main/java/cn/tedu/sys/entity/SysUsers.java
@@ -41,7 +41,7 @@ public class SysUsers implements Serializable {
      */
     private Object valid;
 
-    private Integer deptid;
+    private Integer deptId;
     /**
      * 创建时间
      */
@@ -57,103 +57,7 @@ public class SysUsers implements Serializable {
     /**
      * 修改用户
      */
-    private String modifieduser;
+    private String modifiedUser;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public Object getValid() {
-        return valid;
-    }
-
-    public void setValid(Object valid) {
-        this.valid = valid;
-    }
-
-    public Integer getDeptid() {
-        return deptid;
-    }
-
-    public void setDeptid(Integer deptid) {
-        this.deptid = deptid;
-    }
-
-    public Date getCreatedtime() {
-        return createdtime;
-    }
-
-    public void setCreatedtime(Date createdtime) {
-        this.createdtime = createdtime;
-    }
-
-    public Date getModifiedtime() {
-        return modifiedtime;
-    }
-
-    public void setModifiedtime(Date modifiedtime) {
-        this.modifiedtime = modifiedtime;
-    }
-
-    public String getCreateduser() {
-        return createduser;
-    }
-
-    public void setCreateduser(String createduser) {
-        this.createduser = createduser;
-    }
-
-    public String getModifieduser() {
-        return modifieduser;
-    }
-
-    public void setModifieduser(String modifieduser) {
-        this.modifieduser = modifieduser;
-    }
 
 }

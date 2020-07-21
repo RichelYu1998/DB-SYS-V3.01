526f78dd2e65f305906fc0c89190ac9f2e7ce55f
余浩 <980187223@qq.com>
2020-07-21 18:50:51 +0800

    日志管理_aop添加

--- src/main/java/cn/tedu/common/aspect/SysLogAspect.java
+++ src/main/java/cn/tedu/common/aspect/SysLogAspect.java
@@ -1,31 +1,34 @@
 package cn.tedu.common.aspect;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import cn.tedu.common.annotation.RequiredLog;
 import cn.tedu.common.util.IPUtils;
 import cn.tedu.sys.entity.SysLogs;
 import cn.tedu.sys.service.SysLogsService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.io.JsonEOFException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
-import java.util.Date;
+import javax.annotation.Resource;
 
-@Aspect
+
+/**
+ * @Aspect 注解描述的类型为Spring AOP中的切面对象类型。此对象中可以封装：
+ * 1)切入点(定义在哪些目标对象的哪些方法上进行功能扩展)
+ * 2)通知(封装功能扩展的业务逻辑)
+ */
 //@Slf4j
+@Order(1)
+@Aspect
 @Component
-
 public class SysLogAspect {
     private static final Logger log =
             LoggerFactory.getLogger(SysLogAspect.class);
@@ -36,10 +39,11 @@ public class SysLogAspect {
      * 	这里的涵义是当sysUserServiceImpl对象中的任意方法执行时，都由本切面
      * 	对象的通知方法做功能增强。
      */
-    //@Pointcut("bean(sysUsersService)")
-    @Pointcut("bean(sysLogsService)")
+    @Pointcut("bean(sysUserServiceImpl)")
+    //注解方式的切入点表达式
+    //@Pointcut("@annotation(com.cy.pj.common.annotation.RequiredLog)")
     public void doLogPointCut() {}//此方法中不需要写任何代码
-    private SysLogsService sysLogsService;
+
     /**由@Around注解描述的方法为一个环绕通知方法，我们可以在此方法内部
      * 手动调用目标方法(通过连接点对象ProceedingJoinPoint的proceed方法进行调用)
      * 环绕通知：此环绕通知使用的切入点为bean(sysUserServiceImpl)
@@ -51,20 +55,21 @@ public class SysLogAspect {
      2)应用：
      a)目标方法执行之前或之后都可以进行功能拓展
      b)相对于其它通知优先级最高。
-     @param jointPoint 为一个连接对象(封装了正在要执行的目标方法信息)
+     @param jp 为一个连接对象(封装了正在要执行的目标方法信息)
      @return 目标方法的执行结果
      */
     @Around(value="doLogPointCut()")
-    public Object around(ProceedingJoinPoint jointPoint)throws Throwable{
-        log.info("method start {}",System.currentTimeMillis());
-            long startTime = System.currentTimeMillis();
+    public Object around(ProceedingJoinPoint jp)throws Throwable{
+        System.out.println("SysLogAspect.@Around.before");
+        long start=System.currentTimeMillis();
+        log.info("method start {}",start);
         try {
-            Object result=jointPoint.proceed();//最终会执行目标方法(sysUserServiceImpl对象中的方法)
-            log.info("method end {}",System.currentTimeMillis());
-            long endTime = System.currentTimeMillis();
-            long totalTime=endTime-startTime;
-            log.info("方法执行的总时长为："+totalTime);
-            saveSysLog(jointPoint,totalTime);
+            Object result=jp.proceed();//最终会执行目标方法(sysUserServiceImpl对象中的方法)
+            long end=System.currentTimeMillis();
+            log.info("method end {}",end);
+            System.out.println("SysLogAspect.Around.after");
+            //将用户的正常的行为信息写入到数据库
+            saveUserLog(jp,(end-start));
             return result;
         }catch(Throwable e) {
             log.error("method error {},error msg is {}",
@@ -72,35 +77,37 @@ public class SysLogAspect {
             throw e;
         }
     }
-
-    private void saveSysLog(ProceedingJoinPoint point, long totalTime) throws NoSuchMethodException,SecurityException, JsonProcessingException {
-        //1.获取日志信息
-        MethodSignature ms = (MethodSignature) point.getSignature();
-        Class<?> targetClass = point.getTarget().getClass();
-        String className = targetClass.getName();
-        //获取接口声明的方法
-        String methodName = ms.getMethod().getName();
-        Class<?>[] parameterTypes = ms.getMethod().getParameterTypes();
-        //获取目标对象方法(AOP 版本不同,可能获取方法对象方式也不同)
-        Method targetMethod = targetClass.getDeclaredMethod(methodName, parameterTypes);
-        //获取用户名,学完 shiro 再进行自定义实现,没有就先给固定值
-        String username="admin";
-        //获取方法参数
-        Object[] paramsObj=point.getArgs();
-        System.out.println("paramsObj="+paramsObj);
-        //将参数转换为字符串
-        String params = new ObjectMapper().writeValueAsString(paramsObj);
-        //2.封装日志信息
-        SysLogs log = new SysLogs();
-        log.setUsername(username);//登陆的用户
-        //假如目标方法对象上有注解,我们获取注解定义的操作值
-        targetMethod.getDeclaredAnnotation(RequiredLog.class);
-        log.setMethod(className+"."+methodName);//className.methodName()
-        log.setParams(params);//method params
-        log.setIp(IPUtils.getIpAddr());//ip 地址
-        log.setTime(totalTime);//
-        log.setCreatedTime(new Date());
-        //3.保存日志信息
+    @Resource
+    private SysLogsService sysLogsService;
+    private void saveUserLog(ProceedingJoinPoint jp,long time)throws Exception {
+        //1.获取用户行为日志信息
+        //获取目标对象(要执行的那个目标业务对象)类型
+        Class<?> targetCls=jp.getTarget().getClass();
+        //获取方法签名对象(此对象中封装了要执行的目标方法信息)
+        MethodSignature ms=(MethodSignature) jp.getSignature();
+        //获取目标方法对象，基于此对象获取方法上的RequiredLog注解，进而取到操作名
+        Method targetMethod=targetCls.getMethod(ms.getName(),ms.getParameterTypes());
+        RequiredLog required=targetMethod.getAnnotation(RequiredLog.class);
+        //获取操作名
+        String operation="operation";
+        if(required!=null) {//注解方式的切入点无须做此判断
+            operation=required.value();
+        }
+        //获取目标方法类全名
+        String targetMethodName=targetCls.getName()+"."+ms.getName();
+        //2.构建用户行为日志对象封装用户行为信息
+        SysLogs log=new SysLogs();
+        log.setIp(IPUtils.getIpAddr());
+        log.setUsername("liuqing");//将来的登陆用户，现在可以先写个假数据
+        log.setOperation(operation);//不知道该写什么
+        log.setMethod(targetMethodName);//类全名.方法名
+        log.setParams(Arrays.toString(jp.getArgs()));
+        log.setTime(time);
+        log.setCreatedTime(new java.util.Date());
+        //3.用户行为日志写入到数据库
         sysLogsService.saveObject(log);
     }
+
 }
+
+

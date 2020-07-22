0ab6850a03aaa61c9fe26e3767d975c73a02f851
余浩 <980187223@qq.com>
2020-07-22 09:44:01 +0800

    Spring AOP 异步操作实现

--- src/main/java/cn/tedu/DemoApplication.java
+++ src/main/java/cn/tedu/DemoApplication.java
@@ -2,7 +2,9 @@ package cn.tedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync //spring 容器启动时会创建线程池
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/cn/tedu/sys/service/impl/SysLogsServiceImpl.java
+++ src/main/java/cn/tedu/sys/service/impl/SysLogsServiceImpl.java
@@ -5,7 +5,10 @@ import cn.tedu.sys.dao.SysLogsDao;
 import cn.tedu.sys.entity.PageObject;
 import cn.tedu.sys.entity.SysLogs;
 import cn.tedu.sys.service.SysLogsService;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -83,46 +86,49 @@ public class SysLogsServiceImpl implements SysLogsService {
 
     @Override
     public PageObject<SysLogs> findPageObjects(String username,
-                                              Integer pageCurrent) {
+                                               Integer pageCurrent) {
         //1.参数校验
         //说明：对于所有的检验，非空校验一定要放在第一步。
-        if(pageCurrent==null||pageCurrent<1)
+        if (pageCurrent == null || pageCurrent < 1)
             throw new IllegalArgumentException("页码值不正确");
         //2.查询总记录数
-        int rowCount=sysLogsDao.getRowCount(username);
-        if(rowCount==0)
+        int rowCount = sysLogsDao.getRowCount(username);
+        if (rowCount == 0)
             throw new ServiceException("没有找到对应的记录");
         //3.查询当前页要呈现的记录
-        int pageSize=3;//页面大小(每页最多呈现多少条记录)
-        int startIndex=(pageCurrent-1)*pageSize;//计算起始位置
-        List<SysLogs> records=
+        int pageSize = 3;//页面大小(每页最多呈现多少条记录)
+        int startIndex = (pageCurrent - 1) * pageSize;//计算起始位置
+        List<SysLogs> records =
                 sysLogsDao.findPageObjects(username, startIndex, pageSize);
         //4.对数据进行封装并返回
         //说明：构建对象时，参数的顺序是怎样的要结合你的构造方法的定义
-        return new PageObject<>(pageCurrent, pageSize,rowCount, records);
+        return new PageObject<>(pageCurrent, pageSize, rowCount, records);
     }
 
     @Override
     public int deleteObjects(Integer... ids) {
         //1.参数校验
-        if(ids==null||ids.length==0)
+        if (ids == null || ids.length == 0)
             throw new IllegalArgumentException("参数值无效");
         //2.基于id删除记录
-        int rows=-1;
+        int rows = -1;
         try {
-            rows=sysLogsDao.deleteObjects(ids);//后续对这样的方法调用也要进行异常监控
-        }catch(Throwable e) {
+            rows = sysLogsDao.deleteObjects(ids);//后续对这样的方法调用也要进行异常监控
+        } catch (Throwable e) {
             //报警，给运维人员发短信。
             throw new ServiceException("远端数据访问暂时维护中");
         }
         //3.结果校验并返回
-        if(rows==0)
+        if (rows == 0)
             throw new ServiceException("记录可能已经不存在");
         return rows;
     }
+
     /*
      * 保存日志
      * */
+    @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Override
     public void saveObject(SysLogs entity) {
         sysLogsDao.insertObject(entity);

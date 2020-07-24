7b37cd8237c10095a77f4314cc24d41b387e3ab3
余浩 <980187223@qq.com>
2020-07-24 12:43:57 +0800

    shiro入门

---
+++ src/main/java/cn/tedu/common/config/SpringShiroConfig.java
@@ -0,0 +1,35 @@
+package cn.tedu.common.config;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+
+@Configuration
+public class SpringShiroConfig {
+    @Bean
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager sManager=
+                new DefaultWebSecurityManager();
+        return sManager;
+    }
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager){
+        ShiroFilterFactoryBean sfBean = new ShiroFilterFactoryBean();
+        sfBean.setSecurityManager(securityManager);
+        //定义 map 指定请求过滤规则(哪些资源允许匿名访问,哪些必须认证访问)
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        //静态资源允许匿名访问:"anon"
+        map.put("/bower_components/**","anon");
+        map.put("/modules/**","anon");
+        map.put("/dist/**","anon");
+        map.put("/plugins/**","anon");
+        //除了匿名访问的资源,其它都要认证("authc")后访问
+        map.put("/**","authc");
+        sfBean.setFilterChainDefinitionMap(map);
+        return sfBean;
+    }
+}
